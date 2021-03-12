655252222d10c4fa1d2aea9e36e1e06b1c05b884
root <ricamatheo98@gmail.com>
2021-03-12 21:54:18 +0000

    Fixed Alexnet model

--- Tensorflow/Images/AlexNet.py
+++ Tensorflow/Images/AlexNet.py
@@ -10,7 +10,9 @@ import tensorflow as tf
 def model_creation(isAConnect=False,Wstd=0,Bstd=0):
 	if(not(isAConnect)):
 		model = tf.keras.models.Sequential([
-			tf.keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
+			tf.keras.layers.InputLayer(input_shape=[32,32,3]),
+			tf.keras.layers.experimental.preprocessing.Resizing(227,227),           
+			tf.keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu'),
 			tf.keras.layers.BatchNormalization(),
 			tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
 			tf.keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
@@ -31,11 +33,10 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0):
 			tf.keras.layers.Dense(10, activation='softmax')
 	    ])
 	else:
-		print("1")
-		"""
 
 		model = tf.keras.models.Sequential([
-            tf.keras.InputLayer(input_shape=[224,224]),
+			tf.keras.layers.InputLayer(input_shape=[32,32,3]),
+			tf.keras.layers.experimental.preprocessing.Resizing(227,227),    
 		    ConvAConnect.ConvAConnect(filters=96, kernel_size=(11,11),Wstd=Wstd,Bstd=Bstd, strides=4,padding="VALID"),
             tf.keras.layers.ReLU(),
 		    tf.keras.layers.BatchNormalization(),
@@ -63,53 +64,23 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0):
 		    tf.keras.layers.Dropout(0.5),
 		    AConnect.AConnect(10, Wstd=Wstd,Bstd=Bstd),
             tf.keras.layers.Softmax()
-	    ])"""
+	    ])
 
 
 	return model
-def process_images(image, label):
-    # Normalize images to have a mean of 0 and standard deviation of 1
-	image = tf.image.per_image_standardization(image)
-    # Resize images from 32x32 to 277x277
-	image = tf.image.resize(image, (224,224))
-	return image, label	
 	
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()	
-#CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-#validation_images, validation_labels = train_images[:5000], train_labels[:5000]
-#train_images, train_labels = train_images[5000:], train_labels[5000:]
-#train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-#test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-#validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
-#train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
-#test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
-#validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
-#print("Training data size:", train_ds_size)
-#print("Test data size:", test_ds_size)
-#print("Validation data size:", validation_ds_size)
-#train_ds = (train_ds
-#                 .map(process_images)
-#                 .shuffle(buffer_size=train_ds_size)
-#                  .batch(batch_size=32, drop_remainder=True))
-#test_ds = (test_ds"""
-"""
+CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-validation_ds = (validation_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))"""
 
 
 
 model=model_creation(isAConnect=False)
-train_images,train_labels=process_images(train_images,train_labels)
 #parametros para el entrenamiento
-model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.optimizers.SGD(lr=0.1,momentum=0.9), metrics=['accuracy'])
+model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.optimizers.SGD(lr=0.01,momentum=0.9), metrics=['accuracy'])
 print(model.summary())
 model.fit(train_images,train_labels,
-          batch_size=32,epochs=10,
+          batch_size=256,epochs=25,
+          validation_split=0.2
           )
 
