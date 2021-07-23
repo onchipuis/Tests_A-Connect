90f689df7f5d8bff5b8157bd8593d65d7c0a7b2c
root <ricamatheo98@gmail.com>
2021-07-23 16:43:31 +0000

    base lenet sim error 0,30,50,70

--- Tensorflow/LeNet5_test.py
+++ Tensorflow/LeNet5_test.py
@@ -11,7 +11,7 @@ from Layers import Conv
 from Layers import FC_quant
 from Layers import dva_fc
 from Layers import dva_conv
-identifier = [True]				#Which network you want to train/test True for A-Connect false for normal LeNet
+identifier = [False]				#Which network you want to train/test True for A-Connect false for normal LeNet
 Sim_err = [0,0.3,0.5,0.7]	#Define all the simulation errors
 Wstd = [0.3,0.5,0.7] 			#Define the stddev for training
 Bstd = Wstd
@@ -50,7 +50,7 @@ for i in range(len(identifier)): #Iterate over the networks
         
     else:
         model,_,_=LeNet5.LeNet5(isAConnect=isAConnect,isBin=isBin)	#Same logic is applied here. But is for normal lenet5
-        optimizer = tf.keras.optimizers.SGD(learning_rate=0.1,momentum=0.9)
+        optimizer = tf.keras.optimizers.SGD(learning_rate=0.01,momentum=0.9)
         name = 'LeNet5'        
         if isBin == "yes": 
             name = name+'_BW'          
