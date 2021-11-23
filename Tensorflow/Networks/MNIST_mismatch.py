aee899b423726a9e08416b7381a52004126235e1
root <ricamatheo98@gmail.com>
2021-11-23 20:53:03 +0000

    results for 8Werr

--- Tensorflow/Networks/MNIST_mismatch.py
+++ Tensorflow/Networks/MNIST_mismatch.py
@@ -81,10 +81,10 @@ def Test_MNIST(opt,imgsize=[28,28],Wstd=0,Bstd=0,isBin="no",pool=1000):
 		
 		model = tf.keras.Sequential([
 			tf.keras.layers.Flatten(input_shape=imgsize),	
-			layers.FC_AConnect(128,Wstd,Bstd,isBin),
+			layers.FC_AConnect(128,Wstd,Bstd,isBin, pool=8),
 			tf.keras.layers.BatchNormalization(),
 			tf.keras.layers.ReLU(),
-			layers.FC_AConnect(10,Wstd,Bstd,isBin),
+			layers.FC_AConnect(10,Wstd,Bstd,isBin, pool=8),
 			tf.keras.layers.Softmax()
 		])
 
