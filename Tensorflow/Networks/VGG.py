f6e44b07a025fa3cbf122f0913fc2eb80040c609
Luis Eduardo Rueda Guerrero <lerugue@gmail.com>
2021-11-27 14:41:45 -0500

    Update

--- Tensorflow/Networks/VGG.py
+++ Tensorflow/Networks/VGG.py
@@ -36,7 +36,7 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0):
 	if(not(isAConnect)):
 		model = tf.keras.models.Sequential([
                         #tf.keras.layers.experimental.preprocessing.Rescaling(1./255, input_shape=(32, 32, 3)),
-			#InputLayer(input_shape=(32,32,3)),
+			InputLayer(input_shape=(32,32,3)),
 			tf.keras.layers.experimental.preprocessing.Resizing(100,100),           
 			Conv2D(filters=64, kernel_size=(3,3),strides=(1,1), activation='relu',padding="same"),
 			BatchNormalization(),
