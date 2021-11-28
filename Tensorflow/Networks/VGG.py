e33343d90827d2d25ece5151b2edc628a4cac9f2
Luis Eduardo Rueda Guerrero <lerugue@gmail.com>
2021-11-27 21:12:41 -0500

    Back to 1FC layer with 256 activations in VGG-16.

--- Tensorflow/Networks/VGG.py
+++ Tensorflow/Networks/VGG.py
@@ -71,10 +71,7 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0):
 			MaxPool2D(pool_size=(2,2), strides=(2,2)),
 			Flatten(),
 			Dropout(0.5),
-			Dense(512, activation='relu'),
-			BatchNormalization(),
-			Dropout(0.5),
-			Dense(512, activation='relu'),
+			Dense(256, activation='relu'),
 			BatchNormalization(),
 			Dropout(0.5),
 			Dense(10, activation='softmax')
