4e05dd817c7f518767780ae54b5734c27fa2e5be
Luis Eduardo Rueda Guerrero <lerugue@gmail.com>
2021-11-28 10:20:53 -0500

    Update

--- Tensorflow/Networks/vgg_test.py
+++ Tensorflow/Networks/vgg_test.py
@@ -36,7 +36,7 @@ for j in range(len(Sim_err)):
     if Err == 0:
         N = 1
     else:
-        N = 1000
+        N = 100
             #####
     elapsed_time = time.time() - start_time
     print("Elapsed time: {}".format(hms_string(elapsed_time)))
