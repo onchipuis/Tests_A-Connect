be4fd53d5338293634a7e7f5714c63ce39e14b27
Luis Eduardo Rueda Guerrero <lerugue@gmail.com>
2021-11-29 21:12:39 -0500

    Update

--- Tensorflow/Networks/VGG.py
+++ Tensorflow/Networks/VGG.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 import time
 from datetime import datetime
 #from .aconnect.aconnect.layers import Conv_AConnect, FC_AConnect 
-from .aconnect.aconnect import layers, scripts 
+from ..aconnect.aconnect import layers, scripts 
 from tensorflow.keras.layers import InputLayer, Conv2D, Dense, MaxPool2D, Flatten
 from tensorflow.keras.layers import BatchNormalization, Dropout, ReLU, Softmax
 #import math
