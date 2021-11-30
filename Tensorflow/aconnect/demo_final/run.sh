20407a64125412f64244ead1e6d23f0938e6cc4f
Luis Eduardo Rueda Guerrero <lerugue@gmail.com>
2021-11-30 06:25:23 -0500

    Update

--- Tensorflow/aconnect/demo_final/run.sh
+++
@@ -1,8 +0,0 @@
-python3 app.py  
-python3 ACon_live.py w Image_demo.json
-python3 app.py
-python3 ACon_live.py w Image_demo.json
-python3 app.py
-python3 ACon_live.py w Image_demo.json
-python3 app.py
-python3 ACon_live.py w Image_demo.json 
