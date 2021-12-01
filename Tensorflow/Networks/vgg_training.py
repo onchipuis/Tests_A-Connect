341ef766b4005aa4565aa7bfbceddfe730297ba7
Luis Eduardo Rueda Guerrero <lerugue@gmail.com>
2021-12-01 12:12:04 -0500

    Update

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -31,7 +31,7 @@ def get_top_n_score(target, prediction, n):
     return np.mean(precision)
 
 # LOADING DATASET:
-(X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.cifar10.load_data()	
+(X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.cifar10.load_data()    
 """
 # prepare data augmentation configuration
 train_datagen = ImageDataGenerator(
@@ -53,48 +53,46 @@ model_aux=tf.keras.applications.VGG16(weights="imagenet", include_top=False,
 #### RUN TRAINING FOR DIFFERENT LEVEL OF STOCHASTICITY
 #Wstd_err = [0.3, 0.5, 0.7, 0.8]
 Wstd_err = [0.65]
-pool = [1]
+pool = [1666666]
+isAConnect = True
 
 for j in range(len(Wstd_err)):
     for i in range(len(pool)):
         Err = Wstd_err[j]
         # CREATING NN:
-        model = vgg.model_creation(isAConnect=False,
+        model = vgg.model_creation(isAConnect=isAConnect,
                                     Wstd=Err,Bstd=Err,
                                     FC_pool=pool[i],
                                     errDistr="lognormal")
-
-        # Without Aconnect
-        model.layers[1].set_weights(model_aux.layers[1].get_weights())
-        model.layers[3].set_weights(model_aux.layers[2].get_weights())
-        model.layers[6].set_weights(model_aux.layers[4].get_weights())
-        model.layers[8].set_weights(model_aux.layers[5].get_weights())
-        model.layers[11].set_weights(model_aux.layers[7].get_weights())
-        model.layers[13].set_weights(model_aux.layers[8].get_weights())
-        model.layers[15].set_weights(model_aux.layers[9].get_weights())
-        model.layers[18].set_weights(model_aux.layers[11].get_weights())
-        model.layers[20].set_weights(model_aux.layers[12].get_weights())
-        model.layers[22].set_weights(model_aux.layers[13].get_weights())
-        model.layers[25].set_weights(model_aux.layers[15].get_weights())
-        model.layers[27].set_weights(model_aux.layers[16].get_weights())
-        model.layers[29].set_weights(model_aux.layers[17].get_weights())
-        """
-
-        ## With Aconnect
-        model.layers[1].set_weights(model_aux.layers[1].get_weights())
-        model.layers[4].set_weights(model_aux.layers[2].get_weights())
-        model.layers[8].set_weights(model_aux.layers[4].get_weights())
-        model.layers[11].set_weights(model_aux.layers[5].get_weights())
-        model.layers[15].set_weights(model_aux.layers[7].get_weights())
-        model.layers[18].set_weights(model_aux.layers[8].get_weights())
-        model.layers[21].set_weights(model_aux.layers[9].get_weights())
-        model.layers[25].set_weights(model_aux.layers[11].get_weights())
-        model.layers[28].set_weights(model_aux.layers[12].get_weights())
-        model.layers[31].set_weights(model_aux.layers[13].get_weights())
-        model.layers[35].set_weights(model_aux.layers[15].get_weights())
-        model.layers[38].set_weights(model_aux.layers[16].get_weights())
-        model.layers[41].set_weights(model_aux.layers[17].get_weights())
-        """
+        
+        if (isAConnect==True): # With Aconnect
+            model.layers[1].set_weights(model_aux.layers[1].get_weights())
+            model.layers[4].set_weights(model_aux.layers[2].get_weights())
+            model.layers[8].set_weights(model_aux.layers[4].get_weights())
+            model.layers[11].set_weights(model_aux.layers[5].get_weights())
+            model.layers[15].set_weights(model_aux.layers[7].get_weights())
+            model.layers[18].set_weights(model_aux.layers[8].get_weights())
+            model.layers[21].set_weights(model_aux.layers[9].get_weights())
+            model.layers[25].set_weights(model_aux.layers[11].get_weights())
+            model.layers[28].set_weights(model_aux.layers[12].get_weights())
+            model.layers[31].set_weights(model_aux.layers[13].get_weights())
+            model.layers[35].set_weights(model_aux.layers[15].get_weights())
+            model.layers[38].set_weights(model_aux.layers[16].get_weights())
+            model.layers[41].set_weights(model_aux.layers[17].get_weights())
+        else: # Without Aconnect
+            model.layers[1].set_weights(model_aux.layers[1].get_weights())
+            model.layers[3].set_weights(model_aux.layers[2].get_weights())
+            model.layers[6].set_weights(model_aux.layers[4].get_weights())
+            model.layers[8].set_weights(model_aux.layers[5].get_weights())
+            model.layers[11].set_weights(model_aux.layers[7].get_weights())
+            model.layers[13].set_weights(model_aux.layers[8].get_weights())
+            model.layers[15].set_weights(model_aux.layers[9].get_weights())
+            model.layers[18].set_weights(model_aux.layers[11].get_weights())
+            model.layers[20].set_weights(model_aux.layers[12].get_weights())
+            model.layers[22].set_weights(model_aux.layers[13].get_weights())
+            model.layers[25].set_weights(model_aux.layers[15].get_weights())
+            model.layers[27].set_weights(model_aux.layers[16].get_weights())
+            model.layers[29].set_weights(model_aux.layers[17].get_weights())
 
         #print(model.summary())
 
