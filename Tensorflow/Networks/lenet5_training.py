f1184f655e6b6bdc97c3f1f9ef12edb0085bb2d2
Luis E. Rueda G <lerugue@gmail.com>
2022-01-26 11:29:48 -0500

    Update

--- Tensorflow/Networks/lenet5_training.py
+++ Tensorflow/Networks/lenet5_training.py
@@ -51,7 +51,7 @@ for d in range(len(isAConnect)): #Iterate over the networks
         Wstd_aux = Wstd_err
         FC_pool_aux = FC_pool
         Conv_pool_aux = Conv_pool
-    else
+    else:
         Wstd_aux = 0
         FC_pool_aux = 0
         Conv_pool_aux = 0
