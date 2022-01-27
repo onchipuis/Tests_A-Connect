de6f00ce6763d50b87c0d704770b72783ea88de3
Luis E. Rueda G <lerugue@gmail.com>
2022-01-27 14:59:15 -0500

    Update

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 import VGG as vgg
 import time
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from aconnect import layers, scripts
+from .aconnect import layers, scripts
 #from keras.callbacks import LearningRateScheduler
 custom_objects = {'Conv_AConnect':layers.Conv_AConnect,'FC_AConnect':layers.FC_AConnect}
 
