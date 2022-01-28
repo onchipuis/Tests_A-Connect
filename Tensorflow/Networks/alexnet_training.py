2851a1ac9c33bc22f0ead7c1dcfb3c78bfc6adfe
Luis E. Rueda G <lerugue@gmail.com>
2022-01-28 16:05:05 -0500

    Update

--- Tensorflow/Networks/alexnet_training.py
+++ Tensorflow/Networks/alexnet_training.py
@@ -41,6 +41,7 @@ def get_top_n_score(target, prediction, n):
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
 Wstd_err = [0.3,0.5,0.7]   # Define the stddev for training
 FC_pool = [1,2,4,8,16,32]
+FC_pool = [1]
 Conv_pool = FC_pool
 isBin = ["no"]		    # Do you want binary weights?
 #errDistr = "lognormal"
