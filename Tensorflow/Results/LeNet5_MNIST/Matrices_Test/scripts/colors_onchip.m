b63aeb761c2e8f70947e8774fd434432302beec4
Luis E. Rueda G <lerugue@gmail.com>
2022-01-28 18:56:36 -0500

    Update

---
+++ Tensorflow/Results/LeNet5_MNIST/Matrices_Test/scripts/colors_onchip.m
@@ -0,0 +1,12 @@
+% Invoke this script to use these variables
+% for the colorscheme used in OnChip.
+blue_line = [0 0 255]/255;
+blue_fill = [215 227 240]/255;
+red_line = [255 0 0]/255;
+red_fill = [255 213 213]/255;
+violet_line = [144 44 90]/255;
+violet_fill = [218 134 173]/255;
+green_line = [44 189 110]/255;
+green_fill = [160 233 193]/255;
+gray_line = 128*[1 1 1]/255;
+gray_fill = 211*[1 1 1]/255;
