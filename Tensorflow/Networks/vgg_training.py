40bc751340d9dd2ce17da2b1802a140a02ea89ee
Luis E. Rueda G <lerugue@gmail.com>
2022-01-31 07:31:00 -0500

    Le915331

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -55,8 +55,8 @@ model_aux=tf.keras.applications.VGG16(weights="imagenet", include_top=False,inpu
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
 Wstd_err = [0.7]   # Define the stddev for training
-Conv_pool = [2,8,16]
-FC_pool = [2,4,4]
+Conv_pool = [16]
+FC_pool = [4]
 isBin = ["no"]		    # Do you want binary weights?
 #errDistr = "lognormal"
 errDistr = ["normal"]
@@ -90,13 +90,8 @@ for d in range(len(isAConnect)): #Iterate over the networks
         Conv_pool_aux = [0]
         
     for i in range(len(Conv_pool_aux)):
-        for j in range(len(Wstd_aux)):
-            if Wstd_aux[j]==0.3:
-                po = [1]
-            else:
-                po = range(len(errDistr))
-            
-            for k in po:
+                        
+            for k in range(len(errDistr)):
                 Err = Wstd_aux[j]
                 # CREATING NN:
                 #model_aux = tf.keras.models.load_model(net,custom_objects = custom_objects)
