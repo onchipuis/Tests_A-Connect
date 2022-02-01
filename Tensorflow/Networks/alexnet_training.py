174a34823013cbce417176c46fce8d4dc60a0da6
rvergel <ricamatheo98@gmail.com>
2022-02-01 10:08:07 -0500

    fixed data augmentation

--- Tensorflow/Networks/alexnet_training.py
+++ Tensorflow/Networks/alexnet_training.py
@@ -53,15 +53,16 @@ def normalization(train_images, test_images):
 #### DATA AUGMENTATION
 
 datagen = ImageDataGenerator(
-        featurewise_center=True,  # set input mean to 0 over the dataset
-        samplewise_center=True,  # set each sample mean to 0
-        featurewise_std_normalization=True,  # divide inputs by std of the dataset
-        samplewise_std_normalization=True,  # divide each input by its std
-        zca_whitening=True,  # apply ZCA whitening
-        rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
+        zoom_range=0.1
         vertical_flip=False)  # randomly flip images
     # (std, mean, and principal components if ZCA whitening is applied).
 datagen.fit(X_train)
