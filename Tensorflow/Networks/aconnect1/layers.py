37f47fbe623773ae1cab2f5882a25de6f369ef21
Luis E. Rueda G <lerugue@gmail.com>
2022-02-01 11:47:51 -0500

    Update

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -272,10 +272,9 @@ class FC_AConnect(tf.keras.layers.Layer):
             y = tf.quantization.fake_quant_with_min_max_vars(inputs=x,min=-1,max=1,num_bits=self.bw[0])
             y = tf.cast(y,self.d_type)
             def grad(dy):
-                xi = tf.cast(x,tf.dtypes.float32)
-                xq = tf.quantization.fake_quant_with_min_max_vars(inputs=xi,min=-1,max=1,num_bits=self.bw[0])
-                xq = tf.cast(xq,self.d_type)
+                xq = tf.quantization.fake_quant_with_min_max_vars(inputs=x,min=-1,max=1,num_bits=self.bw[0])
                 xe = tf.divide(xq,x+1e-5)
+                xe = tf.cast(xe,self.d_type)
                 dydx = tf.multiply(dy,xe)
                 return dydx
             return y,grad
