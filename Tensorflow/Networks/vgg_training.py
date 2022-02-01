8d24dd2a7a6a9cf4027eee1f345599924f02ea42
Luis E. Rueda G <lerugue@gmail.com>
2022-02-01 08:39:20 -0500

    Update

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -55,8 +55,8 @@ model_aux=tf.keras.applications.VGG16(weights="imagenet", include_top=False,inpu
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
 Wstd_err = [0.7]   # Define the stddev for training
-Conv_pool = [8]
-FC_pool = [4]
+Conv_pool = [1]
+FC_pool = [1]
 WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
 Wbw = [8]
