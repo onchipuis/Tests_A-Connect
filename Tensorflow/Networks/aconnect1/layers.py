77cdbbdee50d8d83726a89315a32e73af69083f6
Luis E. Rueda G <lerugue@gmail.com>
2022-02-01 19:22:42 -0500

    Update

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -276,7 +276,7 @@ class FC_AConnect(tf.keras.layers.Layer):
             else:
                 xi = tf.cast(x,tf.dtypes.float32)
                 #limit = 2*tf.math.reduce_std(xi)
-                limit = 0.5
+                limit = 10
                 xq = tf.quantization.fake_quant_with_min_max_vars(inputs=xi,min=-limit,max=limit,num_bits=self.bw[0])
                 y = tf.cast(xq,self.d_type)
                 def grad(dy):
@@ -648,7 +648,7 @@ class Conv_AConnect(tf.keras.layers.Layer):
             else:
                 xi = tf.cast(x,tf.dtypes.float32)
                 #limit = 2*tf.math.reduce_std(xi)
-                limit = 0.5
+                limit = 10
                 xq = tf.quantization.fake_quant_with_min_max_vars(inputs=xi,min=-limit,max=limit,num_bits=self.bw[0])
                 y = tf.cast(xq,self.d_type)
                 def grad(dy):
