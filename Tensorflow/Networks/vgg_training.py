091d07320384318d5f38afc899cabccdd7c40c79
Luis E. Rueda G <lerugue@gmail.com>
2022-02-03 18:31:02 -0500

    Searching for the best Number of matrices in VGG-16 with 32x32 input images and no resizing.

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -75,11 +75,9 @@ model_aux=tf.keras.applications.VGG16(weights="imagenet", include_top=False,inpu
 
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0.3]   # Define the stddev for training
-#Conv_pool = [1,2,4,8,16]
-#FC_pool = [1,2,4,4,4]
-Conv_pool = [2]
-FC_pool = [2]
+Wstd_err = [0.7]   # Define the stddev for training
+Conv_pool = [1,2,4,8,16]
+FC_pool = [1,2,4,4,4]
 WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
 Wbw = [8]
@@ -98,7 +96,7 @@ model_base = tf.keras.models.load_model(net_base,custom_objects=custom_objects)
 learning_rate = 0.1
 momentum = 0.9
 batch_size = 256
-epochs = 5
+epochs = 50
 lr_decay = 1e-6
 lr_drop = 30
 """
