eb5d0ef1709c164814d1d3b1d68b309051714647
Luis E. Rueda G <lerugue@gmail.com>
2022-02-03 08:58:59 -0500

    Update

--- Tensorflow/Networks/vgg_test.py
+++ Tensorflow/Networks/vgg_test.py
@@ -128,7 +128,9 @@ for d in range(len(isAConnect)): #Iterate over the networks
                                 N = 1
                                     #####
                             
-                            model = tf.keras.models.load_model('Base.h5',custom_objects=custom_objects)
+                            name = 'Base'
+                            string = folder_models + name + '.h5'
+                            model = tf.keras.models.load_model(string,custom_objects=custom_objects)
                             model.compile(loss='sparse_categorical_crossentropy',
                                     metrics=['accuracy'])
                                     #optimizer=optimizer, 
