7f03dfc178ad0846234f8d8631d4fb8f876be284
Luis E. Rueda G <lerugue@gmail.com>
2022-02-04 19:55:16 -0500

    Update

--- Tensorflow/Networks/vgg_test.py
+++ Tensorflow/Networks/vgg_test.py
@@ -48,7 +48,7 @@ X_train, X_test = normalization(X_train,X_test)
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
 Wstd_err = [0.7]   # Define the stddev for training
-Sim_err = [0.5,0.7]
+Sim_err = [0.5]
 Conv_pool = [2]
 WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
@@ -160,6 +160,6 @@ for d in range(len(isAConnect)): #Iterate over the networks
                             print('\n\n*********************************************************************')
                             print('\n Simulation started at: ',starttime)
                             print('Simulation finished at: ', endtime)
-                            tf.keras.backend.clear_session()
                             gc.collect()
+                            tf.keras.backend.clear_session()
                             tf.compat.v1.reset_default_graph()
