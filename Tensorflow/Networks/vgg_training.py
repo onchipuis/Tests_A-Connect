007a18707b78a63376d0ce257cc6d36e90d5cb4b
Luis E. Rueda G <lerugue@gmail.com>
2022-02-06 22:29:15 -0500

    Back to common training of VGG-16 at 70%, but with 4 FC error matrices

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -75,9 +75,9 @@ model_aux=tf.keras.applications.VGG16(weights="imagenet", include_top=False,inpu
 
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0.8]   # Define the stddev for training
+Wstd_err = [0.7]   # Define the stddev for training
 Conv_pool = [2]
-FC_pool = [2]
+FC_pool = [4]
 WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
 Wbw = [8]
