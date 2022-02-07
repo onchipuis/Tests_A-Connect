f852cf9a89efe2a2d09a34c95e5422b7aec497e6
Luis E. Rueda G <lerugue@gmail.com>
2022-02-07 12:27:42 -0500

    Update

--- Tensorflow/Networks/vgg_test.py
+++ Tensorflow/Networks/vgg_test.py
@@ -47,8 +47,8 @@ X_train, X_test = normalization(X_train,X_test)
 #### MODEL TESTING WITH MONTE CARLO STAGE ####
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0.7,0.5,0.3]   # Define the stddev for training
-Sim_err = [0,0.3,0.5]
+Wstd_err = [0.7]   # Define the stddev for training
+Sim_err = [0.5]
 Conv_pool = [2]
 WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
