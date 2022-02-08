22260172a2430a19656218be4808a033d2dd5a16
rvergel <ricamatheo98@gmail.com>
2022-02-08 13:45:42 -0500

    aconnect model

--- Tensorflow/Networks/Resnet20_training.py
+++ Tensorflow/Networks/Resnet20_training.py
@@ -21,12 +21,12 @@ num_classes = 10
 subtract_pixel_mean = True
 
 # A-Connect model
-isAConnect = False
+isAConnect = True
 Wstd = 0.3
 Bstd = Wstd
 pool = 2
-Nlayers = [4, 7, 10, 14, 17, 21, 24, 28, 31, 32, 36, 39, 43, 46, 50, 53, 54, 58, 61, 65, 68, 74]
-Nlayers2 = [4, 7, 10, 14, 17, 21, 24, 28, 31, 32, 36, 39, 43, 46, 50, 53, 54, 58, 61, 65, 68, 74]
+Nlayers = [4, 7, 10, 14, 17, 21, 24, 28, 31, 32, 36, 39, 43, 46, 50, 53, 54, 58, 61, 65, 68, 75]
+Nlayers2 = [4, 7, 10, 14, 17, 21, 24, 28, 31, 32, 36, 39, 43, 46, 50, 53, 54, 58, 61, 65, 68, 75]
 net_base = "./Models/ResNet20_CIFAR10/Base.h5"
 model_base = tf.keras.models.load_model(net_base) #Load pretrained model with 89.5%
 # Extra parameters 
