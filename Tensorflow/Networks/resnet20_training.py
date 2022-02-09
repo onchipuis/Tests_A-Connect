1c5e97affcdda263ad6fb330e5b0b7afae11e0f1
Luis E. Rueda G <lerugue@gmail.com>
2022-02-09 10:47:39 -0500

    Update

--- Tensorflow/Networks/resnet20_training.py
+++ Tensorflow/Networks/resnet20_training.py
@@ -145,7 +145,7 @@ lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
 
 callbacks = [lr_reducer, lr_scheduler]
 
-optimizer = tf.keras.optimizers.Adam(lr=0.0)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0)
 #optimizer = tf.optimizers.SGD(learning_rate=0.0, 
 #                            momentum=momentum, nesterov= True, decay = lr_decay) #Define optimizer
 
