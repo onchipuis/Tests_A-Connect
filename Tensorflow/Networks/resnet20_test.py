46a5ca318dc4af2e652e80de5e284f44b04b41f4
Luis E. Rueda G <lerugue@gmail.com>
2022-02-09 17:59:35 -0500

    Test AConnect 30% resnet

--- Tensorflow/Networks/resnet20_test.py
+++ Tensorflow/Networks/resnet20_test.py
@@ -47,10 +47,10 @@ X_train, X_test = normalization(X_train,X_test)
 #### MODEL TESTING WITH MONTE CARLO STAGE ####
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0]   # Define the stddev for training
+Wstd_err = [0.3]   # Define the stddev for training
 Sim_err = [0,0.3,0.5,0.7]
 Conv_pool = [2]
-WisQuant = ["no"]		    # Do you want binary weights?
+WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
 Wbw = [8]
 Bbw = Wbw
