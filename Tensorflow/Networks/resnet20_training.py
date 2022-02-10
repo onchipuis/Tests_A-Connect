ed92067185b58ed1f161f89004b44aa0319eebff
Luis E. Rueda G <lerugue@gmail.com>
2022-02-09 19:02:18 -0500

    Training AConnect 70% resnet20

--- Tensorflow/Networks/resnet20_training.py
+++ Tensorflow/Networks/resnet20_training.py
@@ -74,7 +74,7 @@ input_shape = X_train.shape[1:]
 
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0.5]   # Define the stddev for training
+Wstd_err = [0.7]   # Define the stddev for training
 Conv_pool = [2]
 FC_pool = [2]
 WisQuant = ["yes"]		    # Do you want binary weights?
