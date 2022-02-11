4541a5133c150b19b930ee06320d88198242804a
Luis E. Rueda G <lerugue@gmail.com>
2022-02-11 08:55:57 -0500

    Training lognormal 70% AConnect ResNet-20 with pre-trained weights from normal AConnect 70%

--- Tensorflow/Networks/resnet20_training.py
+++ Tensorflow/Networks/resnet20_training.py
@@ -77,7 +77,7 @@ input_shape = X_train.shape[1:]
 
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0.3,0.5,0.7]   # Define the stddev for training
+Wstd_err = [0.7]   # Define the stddev for training
 Conv_pool = [8]
 FC_pool = [2]
 WisQuant = ["yes"]		    # Do you want binary weights?
@@ -90,9 +90,9 @@ saveModel = True
 model_name = 'ResNet20_CIFAR10/'
 folder_models = './Models/'+model_name
 folder_results = '../Results/'+model_name+'Training_data/'
-#net_base = folder_models+'8Werr_Wstd_70_Bstd_70_8bQuant_normalDistr.h5'
 if isAConnect[0]:
-    net_base = folder_models+'Base'+namev+'.h5'
+    #net_base = folder_models+'Base'+namev+'.h5'
+    net_base = folder_models+'8Werr_Wstd_70_Bstd_70_8bQuant_normalDistr.h5'
     model_base = tf.keras.models.load_model(net_base,custom_objects=custom_objects)
 
 # TRAINING PARAMETERS
