9fc13ef8c8a7ff8a58e574551073f15164343c49
Luis E. Rueda G <lerugue@gmail.com>
2022-02-14 11:06:12 -0500

    Test

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -667,6 +667,6 @@ def Quant_custom(x,bwidth,dtype):
         else:
             xe = tf.divide(y,x+1e-5)
             dydx = tf.multiply(dy,xe)
-            dydx = 5
+            dydx = 0
         return dydx
     return y,grad
