88c8e9388c4f189c0c602b9f3932556abaa27b36
Luis E. Rueda G <lerugue@gmail.com>
2022-02-14 18:53:39 -0500

    Update

--- Tensorflow/Networks/lenet5_training.py
+++ Tensorflow/Networks/lenet5_training.py
@@ -33,7 +33,7 @@ X_test = np.float32(X_test) #Convert it to float32
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
 #Wstd_err = [0.3,0.5,0.7]   # Define the stddev for training
-Wstd_err = [0.3]	    # Define the stddev for training
+Wstd_err = [0.0]	    # Define the stddev for training
 Conv_pool = [2]
 FC_pool = Conv_pool
 WisQuant = ["yes"]		    # Do you want binary weights?
