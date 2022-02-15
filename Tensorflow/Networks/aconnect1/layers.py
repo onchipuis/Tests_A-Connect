0dd0a91b9c90011a665d0c529e59e62645fbd15b
Luis E. Rueda G <lerugue@gmail.com>
2022-02-15 17:43:37 -0500

    Changed limit to 2*sigma

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -677,17 +677,15 @@ def Quant_custom(x,bwidth,dtype):
         else:
             limit = math.sqrt(6/(x.get_shape()[0]+x.get_shape()[1]))
         """
-        limit = 1
-        """
         xStd = tf.math.reduce_std(x)
         xMean = tf.math.reduce_mean(x)
-        limit = xStd
-        xFS = 2*limit
-        """
+        limit = 2*xStd
+        #limit = 1
         xi = tf.cast(x,tf.dtypes.float32)
         xq = tf.quantization.fake_quant_with_min_max_vars(inputs=xi,min=-limit,max=limit,num_bits=bwidth)
         y = tf.cast(xq,dtype)
         """
+        xFS = 2*limit
         Nlevels = 2**bwidth
         xLSB = xFS/Nlevels
         xq = tf.floor(x/xLSB+1)
