6675ab9a85114cec1fc8d23b77e1f751ae69a9e7
Luis E. Rueda G <lerugue@gmail.com>
2022-07-28 09:44:49 -0500

    Update alexnet_test

--- Tensorflow/Networks/alexnet_test.py
+++ Tensorflow/Networks/alexnet_test.py
@@ -40,7 +40,8 @@ def get_top_n_score(target, prediction, n):
 #### MODEL TESTING WITH MONTE CARLO STAGE ####
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0,0.3,0.5,0.7]   # Define the stddev for training
+#Wstd_err = [0,0.3,0.5,0.7]   # Define the stddev for training
+Wstd_err = [0]   # Define the stddev for training
 Sim_err = [0,0.3,0.5,0.7]
 Conv_pool = [8]
 WisQuant = ["yes"]		    # Do you want binary weights?
