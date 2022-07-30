5409bf809e2d725c69aebca808455dc3285252c3
Luis E. Rueda G <lerugue@gmail.com>
2022-07-30 16:36:50 -0500

    Update

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -189,7 +189,7 @@ class FC_AConnect(tf.keras.layers.Layer):
                         b = bias*Berr
                         Z = tf.add(tf.matmul(self.X, w), b)
                         
-                #Z = self.LQuant(Z)
+                Z = self.LQuant(Z)
                 return Z
         
         def slice_batch(self,miniBatch,N,row):
@@ -512,7 +512,7 @@ class Conv_AConnect(tf.keras.layers.Layer):
                         b = bias*Berr
                         Z = b+tf.nn.conv2d(self.X,w,self.strides,self.padding)
                 
-                #Z = self.LQuant(Z)
+                Z = self.LQuant(Z)
                 return Z
         
         def slice_batch(self,weights,miniBatch,N,strides):
@@ -685,9 +685,9 @@ def Quant_custom(x,self):
         if (bwidth==1):
             dydx = tf.divide(dy,abs(x)+e)
         else:
-            #xe = tf.divide(y,x+1e-23)  # Not working
-            #dydx = tf.multiply(dy,xe)
-            dydx = dy
+            xe = tf.divide(y,x+1e-12)  # Not working
+            dydx = tf.multiply(dy,xe)
+            #dydx = dy
         return dydx
     
     return y,grad
