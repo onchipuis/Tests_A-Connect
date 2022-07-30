769273a9a40043bc97ffdca912e043c948c3d17f
Luis E. Rueda G <lerugue@gmail.com>
2022-07-30 10:26:49 -0500

    Update

--- Tensorflow/Networks/resnet20_test.py
+++ Tensorflow/Networks/resnet20_test.py
@@ -48,7 +48,7 @@ X_train, X_test = normalization(X_train,X_test)
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
 Wstd_err = [0.3]   # Define the stddev for training
-Sim_err = [0.1]
+Sim_err = [0.3]
 Conv_pool = [8]
 WisQuant = ["yes"]		    # Do you want binary weights?
 BisQuant = WisQuant 
