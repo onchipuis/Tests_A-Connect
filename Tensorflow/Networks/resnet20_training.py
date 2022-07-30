90fec156a452fba1f4f245c83c1f85f379f5a7cf
Luis E. Rueda G <lerugue@gmail.com>
2022-07-30 16:54:07 -0500

    Update

--- Tensorflow/Networks/resnet20_training.py
+++ Tensorflow/Networks/resnet20_training.py
@@ -142,8 +142,7 @@ optimizer = tf.optimizers.SGD(learning_rate=0.0,
 
 ################################################################
 # TRAINING THE MODEL:
-#general_training(model_int=resnet_v1 if(version==1) else resnet_v2,isAConnect=isAConnect,
-general_training(model_int=resnet_v1,isAConnect=isAConnect,
+general_training(model_int=resnet_v1 if(version==1) else resnet_v2,isAConnect=isAConnect,
                         model_base=model_base,transferLearn=transferLearn,
                         Wstd_err=Wstd_err,
                         WisQuant=WisQuant,BisQuant=BisQuant,
