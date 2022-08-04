d2fe37da077bdb1ed8a4d679266a53c9cdba51af
Luis E. Rueda G <lerugue@gmail.com>
2022-08-04 17:04:50 -0500

    Update

--- Tensorflow/Networks/vgg_training.py
+++ Tensorflow/Networks/vgg_training.py
@@ -40,7 +40,7 @@ saveModel = True
 model_name = 'VGG16_CIFAR10/'
 folder_models = './Models/'+model_name
 if isAConnect[0]:
-    net_base = folder_models+'Wstd_0_Bstd_0.h5'
+    net_base = folder_models+'Wstd_0_Bstd_0_8bQuant.h5'
     #net_base = folder_models+'8Werr_Wstd_70_Bstd_70_8bQuant_normalDistr.h5'
     #net_base = folder_models+'8Werr_Wstd_50_Bstd_50_8bQuant_lognormalDistr.h5'
     model_base = tf.keras.models.load_model(net_base,custom_objects=custom_objects)
