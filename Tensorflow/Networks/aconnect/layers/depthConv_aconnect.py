465097d8fdd6ae8a3dc193706892dcfc5199d87e
Luis E. Rueda G <lerugue@gmail.com>
2022-08-06 08:43:24 -0500

    Update

--- Tensorflow/Networks/aconnect/layers/depthConv_aconnect.py
+++ Tensorflow/Networks/aconnect/layers/depthConv_aconnect.py
@@ -34,7 +34,7 @@ class DepthWiseConv_AConnect(tf.keras.layers.Layer):
                 padding="VALID",
                 data_format='channels_last',
                 #dilation_rate=(1, 1),
-                depth_multiplier=1,
+                depth_multiplier: int=1,
                 in_channels=None,
                 **kwargs):
 
@@ -79,7 +79,7 @@ class DepthWiseConv_AConnect(tf.keras.layers.Layer):
                 print(type(self.in_channels))
                 print(type(self.depth_multiplier))
                 self.filter_shape = [self.kernel_size[0],self.kernel_size[1],self.in_channels, self.depth_multiplier]
-                self.bias_shape = self.in_channels*int(self.depth_multiplier)
+                self.bias_shape = self.in_channels*self.depth_multiplier
 
                 self.W = self.add_weight('kernel',
                                           shape = self.filter_shape,
