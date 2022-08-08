686abe9b78784d9cfbdb0534d91ee698df514c8f
Luis E. Rueda G <lerugue@gmail.com>
2022-08-08 16:54:12 -0500

    Update

--- Tensorflow/Networks/LeNet5.py
+++ Tensorflow/Networks/LeNet5.py
@@ -16,57 +16,58 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
         Conv_pool=8,FC_pool=8,errDistr="normal",
         bwErrProp=True,**kwargs):
 
-        AConnect_args = {"Wstd": Wstd,
-                        "Bstd": Bstd,
-                        "isQuant": isQuant,
-                        "bw": bw,
-                        "errDistr": errDistr,
-                        "bwErrProp": bwErrProp,
-                        "d_type": tf.dtypes.float16}
-	if(not(isAConnect)):
-		model = tf.keras.Sequential([
-			InputLayer(input_shape=[32,32]),
-			Reshape((32,32,1)),
-			Conv2D(6,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh"),
-                        BatchNormalization(),           
-			#Activation('tanh'),           
-                        AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
-			Conv2D(16,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh"),
-                        BatchNormalization(),           
-			#Activation('tanh'),			
-                        AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
-			Flatten(),
-			Dense(120,activation="tanh"),
-                        BatchNormalization(),           
-			#Activation('tanh'),           
-			Dense(84,activation="tanh"),
-                        BatchNormalization(),           
-			#Activation('tanh'),           
-			Dense(10),
-			Softmax()							
-		])
-	else:
-		model = tf.keras.Sequential([
-			InputLayer(input_shape=[32,32]),
-			Reshape((32,32,1)),
-			Conv_AConnect(6,kernel_size=(5,5),strides=1,padding="VALID",pool=Conv_pool,**AConnect_args),
-                        BatchNormalization(),
-			Activation('tanh'),           
-			AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
-			Conv_AConnect(16,kernel_size=(5,5),strides=1,padding="VALID",pool=Conv_pool,**AConnect_args),
-                        BatchNormalization(),           
-			Activation('tanh'),                       
-			AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
-			Flatten(),
-			FC_AConnect(120,pool=FC_pool,**AConnect_args),
-                        BatchNormalization(),           
-			Activation('tanh'),                       
-			FC_AConnect(84,pool=FC_pool,**AConnect_args),
-                        BatchNormalization(),           
-			Activation('relu'),                       
-			FC_AConnect(10,pool=FC_pool,**AConnect_args),
-			Softmax()							
-		])		
-		
-	
-	return model
+    AConnect_args = {"Wstd": Wstd,
+                    "Bstd": Bstd,
+                    "isQuant": isQuant,
+                    "bw": bw,
+                    "errDistr": errDistr,
+                    "bwErrProp": bwErrProp,
+                    "d_type": tf.dtypes.float16}
+
+    if(not(isAConnect)):
+        model = tf.keras.Sequential([
+                InputLayer(input_shape=[32,32]),
+                Reshape((32,32,1)),
+                Conv2D(6,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh"),
+                BatchNormalization(),           
+                #Activation('tanh'),           
+                AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
+                Conv2D(16,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh"),
+                BatchNormalization(),           
+                #Activation('tanh'),			
+                AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
+                Flatten(),
+                Dense(120,activation="tanh"),
+                BatchNormalization(),           
+                #Activation('tanh'),           
+                Dense(84,activation="tanh"),
+                BatchNormalization(),           
+                #Activation('tanh'),           
+                Dense(10),
+                Softmax()							
+        ])
+    else:
+        model = tf.keras.Sequential([
+                InputLayer(input_shape=[32,32]),
+                Reshape((32,32,1)),
+                Conv_AConnect(6,kernel_size=(5,5),strides=1,padding="VALID",pool=Conv_pool,**AConnect_args),
+                BatchNormalization(),
+                Activation('tanh'),           
+                AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
+                Conv_AConnect(16,kernel_size=(5,5),strides=1,padding="VALID",pool=Conv_pool,**AConnect_args),
+                BatchNormalization(),           
+                Activation('tanh'),                       
+                AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
+                Flatten(),
+                FC_AConnect(120,pool=FC_pool,**AConnect_args),
+                BatchNormalization(),           
+                Activation('tanh'),                       
+                FC_AConnect(84,pool=FC_pool,**AConnect_args),
+                BatchNormalization(),           
+                Activation('relu'),                       
+                FC_AConnect(10,pool=FC_pool,**AConnect_args),
+                Softmax()							
+        ])		
+            
+
+    return model
