0a365b83ce6bffa74029246ea874d1e66ae5d7bb
Luis E. Rueda G <lerugue@gmail.com>
2022-08-10 10:46:14 -0500

    Update

--- Tensorflow/Networks/ResNet18.py
+++ Tensorflow/Networks/ResNet18.py
@@ -12,7 +12,7 @@ kaiming_normal = keras.initializers.VarianceScaling(scale=2.0, mode='fan_out', d
 def conv3x3(x, out_planes, stride=1,**AConnect_args):
     #x = layers.ZeroPadding2D(padding=1)(x)
     out = Conv_AConnect(filters=out_planes, kernel_size=(3,3), strides=stride, 
-                    padding="SAME",**AConnect_args)(x)
+                    **AConnect_args)(x)
     return out 
 
 def basic_block(x, planes, stride=1, downsample=None,**AConnect_args):
