3a8494e003c97548cca8648b43bc49d693558bd1
Luis E. Rueda G <lerugue@gmail.com>
2022-08-19 18:12:06 -0500

    Update

--- Tensorflow/Networks/aconnect/layers/depthConv_aconnect.py
+++ Tensorflow/Networks/aconnect/layers/depthConv_aconnect.py
@@ -41,7 +41,7 @@ class DepthWiseConv_AConnect(tf.keras.layers.Layer):
                 #dilation_rate=(1, 1),
 
                 super(DepthWiseConv_AConnect, self).__init__()
-                self.kernel_size = kernel_size #if type(kernel_size)==tuple else (kernel_size,kernel_size)
+                self.kernel_size = kernel_size
                 self.strides = self._strides = strides
                 self.padding = padding
                 self.data_format=data_format
@@ -82,11 +82,13 @@ class DepthWiseConv_AConnect(tf.keras.layers.Layer):
                 
                 ### Compute the shape of the weights. Input shape could be
                 ### [H,W,Ch,depth_mult] RGB
+                """
                 if len(list(self.kernel_size,)) > 1:
                     self.filter_shape = list(self.kernel_size) + list((self.in_channels,self.filters))
                 else:
                     self.filter_shape = list(self.kernel_size) + list(self.kernel_size) + list((self.in_channels,self.filters))
-                #self.filter_shape = [self.kernel_size[0],self.kernel_size[1],self.in_channels, self.depth_multiplier]
+                """
+                self.filter_shape = [self.kernel_size[0],self.kernel_size[1],self.in_channels, self.depth_multiplier]
 
                 self.W = self.add_weight('kernel',
                                           shape = self.filter_shape,
