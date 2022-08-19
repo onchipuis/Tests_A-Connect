cb508079dc890e851a33622be5ba2eeab8f2be5e
Luis E. Rueda G <lerugue@gmail.com>
2022-08-19 18:14:03 -0500

    Update

--- Tensorflow/Networks/resnet18_test.py
+++ Tensorflow/Networks/resnet18_test.py
@@ -22,7 +22,7 @@ X_train, X_test = normalization(X_train,X_test)
 #### MODEL TESTING WITH MONTE CARLO STAGE ####
 # INPUT PARAMTERS:
 isAConnect = [True]   # Which network you want to train/test True for A-Connect false for normal LeNet
-Wstd_err = [0.7]   # Define the stddev for training
+Wstd_err = [0]   # Define the stddev for training
 Sim_err = [0]
 #Conv_pool = [1,2,4,8,16]
 Conv_pool = [16]
